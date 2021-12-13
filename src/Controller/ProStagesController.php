22bc2ec2c9acb454abf9d501f7d91b867ca876dc
Jules Royet <jroyet@iutbayonne.univ-pau.fr>
2021-12-13 13:18:59 +0100

    Ajout du template, modification du controller et des twigs

--- src/Controller/ProStagesController.php
+++ src/Controller/ProStagesController.php
@@ -9,12 +9,35 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProStagesController extends AbstractController
 {
     /**
-     * @Route("/pro/stages", name="pro_stages")
+     * @Route("/", name="prostages_accueil")
      */
     public function index(): Response
     {
-        return $this->render('pro_stages/index.html.twig', [
-            'controller_name' => 'ProStagesController',
-        ]);
+        return $this->render('pro_stages/index.html.twig');
     }
+	
+	/**
+	* @Route ("/entreprises" , name ="prostages_entreprises")
+	*/
+	public function filtrerEntreprises () : Response
+	{
+		return $this->render('pro_stages/entreprises.html.twig');
+	}
+	
+	/**
+	* @Route ("/formations" , name ="prostages_formations")
+	*/
+	public function filtrerFormations () : Response
+	{
+		return $this->render('pro_stages/formations.html.twig');
+	}
+	
+	/**
+	 * @Route ("/stages/{id}" , name ="prostages_stages")
+	 */
+	 public function afficherStages ($id) : Response
+	 {
+		return $this->render('pro_stages/stages.html.twig');
+	 }
 }
+
