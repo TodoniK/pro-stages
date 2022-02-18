6750a8f0abd6e079c7a1d1b259331dd518768bab
Jules Royet <jroyet@iutbayonne.univ-pau.fr>
2022-02-18 13:42:56 +0100

    Modification du theme utilisé par défaut pour les formulaires + correction du controlleur pour séléctionner les formations d'un stage

--- src/Controller/ProStagesController.php
+++ src/Controller/ProStagesController.php
@@ -217,7 +217,6 @@ class ProStagesController extends AbstractController
 			// Envoyer les ressources en BD
 			$manager->persist($stage);
 			$manager->persist($stage->getEntreprise());
-			$manager->persist($stage->getFormation());
 			$manager->flush();
 
 			// Rediriger l'utilisateur vers la page de remerciement
